fa04cd194ebacaf09d377379b978e7696d566313
Andreea Valentina Iote <47303830+Deea95@users.noreply.github.com>
2024-02-01 18:35:59 +0200

    Add files via upload

---
+++ FacebookLogin/src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    // Se declara variabilele:
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }
+}
